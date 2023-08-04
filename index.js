7882111d9c9e0b27d05be36eb5caffe44ee4c511
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-08-04 21:49:58 +0200

    canvis 1

--- index.js
+++ index.js
@@ -18,52 +18,77 @@ app.get("/:num", (req, res) => {
 
 // crida a la api
 app.get("/:num/:maxlen", (req, res) => {
-  try {
-    const num = parseInt(req.params.num);
-    const maxlen = parseInt(req.params.maxlen);
-
-    // Errors en els parametres
-    if (isNaN(num) || num < 1) {
-      res.status(400).json({ error: "Invalid number of phrases" });
-      return;
-    }
-    // sila frase té menys de 30 caracters, és massa curta
-    if (isNaN(maxlen) || maxlen < 30) {
-      res.status(400).json({ error: "Max length too short to generate phrases" });
-      return;
-    }
-
-    let phrases = [];
-    for (let i = 0; i < num; i++) {
-      let randomPhrase;
-      let attempts = 0; // pel bucle do while
-
-      do {
-        const randomPerson = data.person[getRandomIndex(data.person)];
-        const randomAction = data.action[getRandomIndex(data.action)];
-        const randomPlace = data.place[getRandomIndex(data.place)];
-        if (!randomPerson || !randomAction || !randomPlace) {
-          throw new Error("Failed to generate a random phrase");
-        }
-
-        randomPhrase = `${randomPerson.trim()} ${randomAction.trim()} ${randomPlace.trim()}`;
-        attempts++;
-      } while (randomPhrase.length > maxlen && attempts < 5000); // limito el número de intents per evitar un bucle infinit, i tiro l'error a sota.
-
-      if (attempts === 5000) {
-        throw new Error("Failed to generate a phrase within the maximum length");
+    try {
+      const num = parseInt(req.params.num);
+      const maxlen = parseInt(req.params.maxlen);
+  
+      // Errors en els parametres
+      if (isNaN(num) || num < 1) {
+        res.status(400).json({ error: "Invalid number of phrases" });
+        return;
       }
-
-      phrases.push(randomPhrase);
+      // sila frase té menys de 30 caracters, és massa curta
+      if (isNaN(maxlen) || maxlen < 30) {
+        res.status(400).json({ error: "Max length too short to generate phrases" });
+        return;
+      }
+  
+      // Check if it's possible to generate num unique phrases
+      if (num > data.person.length || num > data.action.length || num > data.place.length) {
+        res.status(400).json({ error: "Can't generate the requested number of unique phrases" });
+        return;
+      }
+  
+      let usedPersons = new Set();
+      let usedActions = new Set();
+      let usedPlaces = new Set();
+  
+      let phrases = [];
+      for (let i = 0; i < num; i++) {
+        let randomPerson, randomAction, randomPlace;
+        let randomPhrase;
+        let attempts = 0;
+  
+        do {
+          do {
+            randomPerson = data.person[getRandomIndex(data.person)];
+          } while (usedPersons.has(randomPerson));
+  
+          do {
+            randomAction = data.action[getRandomIndex(data.action)];
+          } while (usedActions.has(randomAction));
+  
+          do {
+            randomPlace = data.place[getRandomIndex(data.place)];
+          } while (usedPlaces.has(randomPlace));
+  
+          if (!randomPerson || !randomAction || !randomPlace) {
+            throw new Error("Failed to generate a random phrase");
+          }
+          const capitalizedRandomPerson = randomPerson.charAt(0).toUpperCase() + randomPerson.slice(1);
+          randomPhrase = `${capitalizedRandomPerson.trim()} ${randomAction.trim()} ${randomPlace.trim()}`;
+          attempts++;
+        } while (randomPhrase.length > maxlen && attempts < 5000);
+  
+        if (attempts === 5000) {
+          throw new Error("Failed to generate a phrase within the maximum length");
+        }
+  
+        usedPersons.add(randomPerson);
+        usedActions.add(randomAction);
+        usedPlaces.add(randomPlace);
+  
+        phrases.push(randomPhrase);
+      }
+  
+      res.json({ phrases: phrases });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: `Error generating the random phrases: [ ${error} ]` });
     }
-    res.json({ phrases: phrases });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: `Error generating the random phrases: [ ${error} ]` });
-  }
-});
+  });
 
 // Iniciar el servidor a 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on port 3000 ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);  
 });
